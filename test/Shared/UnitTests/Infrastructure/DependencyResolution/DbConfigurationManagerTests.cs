89e3bfa5152b6b4d9dcbfb84cfd1e0e3e1594999
mariloutb <52055678+mariloutb@users.noreply.github.com>
2019-07-07 20:56:12 -0400

    Update DbConfigurationManagerTests.cs

--- test/Shared/UnitTests/Infrastructure/DependencyResolution/DbConfigurationManagerTests.cs
+++ test/Shared/UnitTests/Infrastructure/DependencyResolution/DbConfigurationManagerTests.cs
@@ -95,8 +95,8 @@ namespace System.Data.Entity.Infrastructure.DependencyResolution
 
             /// <summary>
             /// This test makes calls from multiple threads such that we have at least some chance of finding threading
-            /// issues. As with any test of this type just because the test passes does not mean that the code is
-            /// correct. On the other hand if this test ever fails (EVEN ONCE) then we know there is a problem to
+            /// issues. As with any test of this type just because the test passes do not mean that the code is
+            /// correct. On the other hand, if this test ever fails (EVEN ONCE) then we know there is a problem to
             /// be investigated. DON'T just re-run and think things are okay if the test then passes.
             /// </summary>
             [Fact]
@@ -107,8 +107,8 @@ namespace System.Data.Entity.Infrastructure.DependencyResolution
 
             /// <summary>
             /// This test makes calls from multiple threads such that we have at least some chance of finding threading
-            /// issues. As with any test of this type just because the test passes does not mean that the code is
-            /// correct. On the other hand if this test ever fails (EVEN ONCE) then we know there is a problem to
+            /// issues. As with any test of this type just because the test passes do not mean that the code is
+            /// correct. On the other hand, if this test ever fails (EVEN ONCE) then we know there is a problem to
             /// be investigated. DON'T just re-run and think things are okay if the test then passes.
             /// </summary>
             [Fact]
@@ -206,8 +206,8 @@ namespace System.Data.Entity.Infrastructure.DependencyResolution
 
             /// <summary>
             /// This test makes calls from multiple threads such that we have at least some chance of finding threading
-            /// issues. As with any test of this type just because the test passes does not mean that the code is
-            /// correct. On the other hand if this test ever fails (EVEN ONCE) then we know there is a problem to
+            /// issues. As with any test of this type just because the test passes do not mean that the code is
+            /// correct. On the other hand, if this test ever fails (EVEN ONCE) then we know there is a problem to
             /// be investigated. DON'T just re-run and think things are okay if the test then passes.
             /// </summary>
             [Fact]
@@ -479,8 +479,8 @@ namespace System.Data.Entity.Infrastructure.DependencyResolution
 
             /// <summary>
             /// This test makes calls from multiple threads such that we have at least some chance of finding threading
-            /// issues. As with any test of this type just because the test passes does not mean that the code is
-            /// correct. On the other hand if this test ever fails (EVEN ONCE) then we know there is a problem to
+            /// issues. As with any test of this type just because the test passes do not mean that the code is
+            /// correct. On the other hand, if this test ever fails (EVEN ONCE) then we know there is a problem to
             /// be investigated. DON'T just re-run and think things are okay if the test then passes.
             /// </summary>
             [Fact]
@@ -493,8 +493,8 @@ namespace System.Data.Entity.Infrastructure.DependencyResolution
 
             /// <summary>
             /// This test makes calls from multiple threads such that we have at least some chance of finding threading
-            /// issues. As with any test of this type just because the test passes does not mean that the code is
-            /// correct. On the other hand if this test ever fails (EVEN ONCE) then we know there is a problem to
+            /// issues. As with any test of this type just because the test passes do not mean that the code is
+            /// correct. On the other hand, if this test ever fails (EVEN ONCE) then we know there is a problem to
             /// be investigated. DON'T just re-run and think things are okay if the test then passes.
             /// </summary>
             [Fact]
@@ -643,8 +643,8 @@ namespace System.Data.Entity.Infrastructure.DependencyResolution
 
             /// <summary>
             /// This test makes calls from multiple threads such that we have at least some chance of finding threading
-            /// issues. As with any test of this type just because the test passes does not mean that the code is
-            /// correct. On the other hand if this test ever fails (EVEN ONCE) then we know there is a problem to
+            /// issues. As with any test of this type just because the test passes do not mean that the code is
+            /// correct. On the other hand, if this test ever fails (EVEN ONCE) then we know there is a problem to
             /// be investigated. DON'T just re-run and think things are okay if the test then passes.
             /// </summary>
             [Fact]
