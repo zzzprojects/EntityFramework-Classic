3d592713b74efd5566ac070ce27b2eee347c4694
Jonathan Magnan <info@zzzprojects.com>
2019-07-14 21:27:31 -0400

    Update InternalDispatcherTests.cs

--- test/Shared/UnitTests/Infrastructure/Interception/InternalDispatcherTests.cs
+++ test/Shared/UnitTests/Infrastructure/Interception/InternalDispatcherTests.cs
@@ -853,7 +853,7 @@ namespace System.Data.Entity.Infrastructure.Interception
             }
 
             [Fact]
-            public void Async_Dispatch_dispatches_to_all_registered_interceptors_even_if_task_is_cancelled()
+            public void Async_Dispatch_dispatches_to_all_registered_interceptors_even_if_task_is_canceled()
             {
                 var mockInterceptors = CreateMockInterceptors(
                     c =>
