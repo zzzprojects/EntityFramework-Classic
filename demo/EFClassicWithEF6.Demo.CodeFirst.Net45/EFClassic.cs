86e7030341fcb554e2b3b84a4073c2a27b703699
Jonathan Magnan <info@zzzprojects.com>
2019-08-13 07:58:39 -0400

    Add EFClassicWIthEF6 demo

---
+++ demo/EFClassicWithEF6.Demo.CodeFirst.Net45/EFClassic.cs
@@ -0,0 +1,105 @@
+﻿// not required if the 'Z.EntityFramework.Classic' dll has the alias 'global'
+extern alias EFClassic;
+using EFClassic::System.Data.Entity;
+
+using System; 
+using System.Linq;
+
+namespace EFClassicWithEF6.Demo.CodeFirst.Net45
+{
+	public class EFClassic
+	{
+		public static void Execute()
+		{
+			Console.WriteLine("EFClassic");
+			Console.WriteLine("");
+			Console.WriteLine("---");
+			Console.WriteLine("");
+
+			// CLEAR
+			using (var context = new EntityContext())
+			{
+				context.Customers.RemoveRange(context.Customers);
+				context.SaveChanges();
+
+				Console.WriteLine("Database clearer...");
+				Console.WriteLine("");
+				Console.WriteLine("---");
+				Console.WriteLine("");
+			}
+
+			// ADD 2 new customers
+			using (var context = new EntityContext())
+			{
+				context.Customers.Add(new Customer { Name = "Customer_A", Description = "Description", IsActive = true });
+				context.Customers.Add(new Customer { Name = "Customer_B", Description = "Description", IsActive = true });
+
+				context.SaveChanges();
+
+				Console.WriteLine("Customers added...");
+			}
+
+			using (var context = new EntityContext())
+			{
+				foreach (var customer in context.Customers.ToList())
+				{
+					Console.WriteLine("");
+					Console.WriteLine("Customer.CustomerID : " + customer.CustomerID);
+					Console.WriteLine("Customer.Name : " + customer.Name);
+					Console.WriteLine("Customer.Description : " + customer.Description);
+					Console.WriteLine("Customer.IsActive : " + customer.IsActive);
+				}
+
+				Console.WriteLine("");
+				Console.WriteLine("---");
+				Console.WriteLine("");
+			}
+
+			using (var context = new EntityContext())
+			{
+				var list = context.Customers.ToList();
+
+				list.First().Description = "Updated_A";
+
+				context.Customers.Add(new Customer { Name = "Customer_C", Description = "Description" });
+
+				context.SaveChanges();
+				Console.WriteLine("Customers added/updated...");
+			}
+
+			using (var context = new EntityContext())
+			{
+				foreach (var customer in context.Customers.ToList())
+				{
+					Console.WriteLine("");
+					Console.WriteLine("Customer.CustomerID : " + customer.CustomerID);
+					Console.WriteLine("Customer.Name : " + customer.Name);
+					Console.WriteLine("Customer.Description : " + customer.Description);
+					Console.WriteLine("Customer.IsActive : " + customer.IsActive);
+				}
+
+				Console.WriteLine("");
+				Console.WriteLine("---");
+				Console.WriteLine("");
+			}
+		}
+
+		public class EntityContext : DbContext
+		{
+			public EntityContext() : base(Program.ConnectionStringEFClassic)
+			{
+			}
+
+			public DbSet<Customer> Customers { get; set; }
+		}
+
+
+		public class Customer
+		{
+			public int CustomerID { get; set; }
+			public string Name { get; set; }
+			public string Description { get; set; }
+			public bool IsActive { get; set; }
+		}
+	}
+}
---
+++ demo/EFClassicWithEF6.Demo.CodeFirst.Net45/Program.cs
@@ -0,0 +1,27 @@
+﻿// not required if the 'Z.EntityFramework.Classic' dll has the alias 'global'
+extern alias EFClassic;
+using EFClassic::Z.EntityFramework.Classic;
+
+using System;
+
+namespace EFClassicWithEF6.Demo.CodeFirst.Net45
+{
+	public class Program
+	{
+		public static string ConnectionStringEF6 = "Server=localhost;Initial Catalog=EF6_Demo;Integrated Security=true;";
+		public static string ConnectionStringEFClassic = "Server=localhost;Initial Catalog=EFClassic_Demo;Integrated Security=true;";
+
+        // See 'https://entityframework-classic.net/using-ef-classic-with-ef-6' to understand about extern alias
+        public static void Main()
+		{ 
+			// see App.Config
+			EntityFrameworkManager.ConfigSectionName = "entityFrameworkClassic";
+
+			EF6.Execute();
+			EFClassic.Execute();
+
+			Console.WriteLine("Press any key.");
+			Console.ReadKey();
+		}
+	}
+}
